6f7ee3f0d6ac73dac14a6d6d78a23749d5dd6ef5
viktorvoltz <vctrchinyeaka@gmail.com>
2022-05-25 16:23:05 +0100

    run

--- scripts/run.js
+++ scripts/run.js
@@ -1,18 +1,26 @@
 const main = async () => {
+    const [owner, randomPerson] = await hre.ethers.getSigners();
     const domainContractFactory = await hre.ethers.getContractFactory('Domains');
     const domainContract = await domainContractFactory.deploy();
     await domainContract.deployed();
     console.log("Contract deployed to:", domainContract.address);
-  };
-  
-  const runMain = async () => {
+    console.log("Contract deployed by:", owner.address);
+
+    const txn = await domainContract.register("voltz");
+    await txn.wait();
+
+    const domainOwner = await domainContract.getAddress("voltz");
+    console.log("Owner of domain:", domainOwner);
+};
+
+const runMain = async () => {
     try {
-      await main();
-      process.exit(0);
+        await main();
+        process.exit(0);
     } catch (error) {
-      console.log(error);
-      process.exit(1);
+        console.log(error);
+        process.exit(1);
     }
-  };
-  
-  runMain();
+};
+
+runMain();
