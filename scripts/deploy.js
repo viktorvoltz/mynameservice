6ff5f6f7bf79310193347ef2601ea301fcf40b82
viktorvoltz <vctrchinyeaka@gmail.com>
2022-05-26 11:11:06 +0100

    pay for domains

---
+++ scripts/deploy.js
@@ -0,0 +1,29 @@
+const main = async () => {
+    const [owner, randomPerson] = await hre.ethers.getSigners();
+    const domainContractFactory = await hre.ethers.getContractFactory('Domains');
+    const domainContract = await domainContractFactory.deploy("voltz");
+    await domainContract.deployed();
+    console.log("Contract deployed to:", domainContract.address);
+    console.log("Contract deployed by:", owner.address);
+
+    let txn = await domainContract.register("mortal", { value: hre.ethers.utils.parseEther('0.1') });
+    await txn.wait()
+
+    const domainOwner = await domainContract.getAddress("mortal");
+    console.log("Owner of domain:", domainOwner);
+
+    const balance = await hre.ethers.provider.getBalance(domainContract.address);
+    console.log("Contract balance:", hre.ethers.utils.formatEther(balance));
+};
+
+const runMain = async () => {
+    try {
+        await main();
+        process.exit(0);
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+};
+
+runMain();
